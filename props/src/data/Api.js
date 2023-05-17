f7370460f9d007450338afb2571bcafc3c8d4870
mehrez1401 <mehrezchaari.1@gmail.com>
2023-05-17 19:53:21 +0200

    props

---
+++ props/src/data/Api.js
@@ -0,0 +1,17 @@
+const apis=[{
+    link: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d2/Eiffelturm.JPG/275px-Eiffelturm.JPG",
+    destination: "tour eiffel",
+    desc: "La tour Eiffel a été construite le 28 janvier 1887 pour l'exposition universelle de Paris de 1889 pour démontrer la puissance industrielle de la France. Elle ne devait rester que 20 ans. Finalement, elle est restée et est devenue le symbole permanent de Paris et de la France.",
+  },
+  {
+    link: "https://www.sciencesetavenir.fr/assets/img/2018/12/02/cover-r4x3w1200-5c0513384a3ea-cf2b8b03c097be75252710c6cc7ef1e6b8d5a32c-jpg.jpg",
+    destination: "tour de pise",
+    desc: "La tour de Pise (en italien : torre di Pisa) est le campanile de la cathédrale Notre-Dame de l’Assomption de Pise, en Toscane (Italie). Elle est située à proximité du chevet de la cathédrale et fait partie des monuments de la piazza dei Miracoli (la « place des Miracles »), inscrit au patrimoine mondial de l'UNESCO. Sa construction débuta en 1173. Mondialement connue, elle est un des symboles de l’Italie et l’emblème de la ville de Pise.",
+  },
+  {
+    link: "https://www.nyc.fr/wp-content/uploads/2015/07/statue_liberte2.jpg",
+    destination: "statue de la liberté",
+    desc: "La statue de la Liberté ou La Liberté éclairant le monde1,2 (en anglais : Liberty Enlightening the World)3, ou simplement Liberté, plus connue sous le nom Statue of Liberty, est l'un des monuments les plus célèbres des États-Unis. Cette statue monumentale est située à New York, sur la Liberty Island, au sud de Manhattan, à l'embouchure de l'Hudson et à proximité d’Ellis Island.",
+  }
+  ]
+  export default apis
